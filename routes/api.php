7dfff897e8f38dc3ead360e4388df278eee8adf6
Nathan McCormick <mccormicknathan28@gmail.com>
2026-06-22 13:20:35 +1200

    Register all API routes for measurements, photos, and coach endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CoachProgressController;
+use App\Http\Controllers\MeasurementController;
+use App\Http\Controllers\ProgressPhotoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Measurements
+    Route::post('/measurements', [MeasurementController::class, 'store']);
+    Route::get('/measurements', [MeasurementController::class, 'index']);
+    Route::put('/measurements/{measurement}', [MeasurementController::class, 'update']);
+    Route::delete('/measurements/{measurement}', [MeasurementController::class, 'destroy']);
+
+    // Progress photos
+    Route::post('/photos', [ProgressPhotoController::class, 'store']);
+    Route::get('/photos', [ProgressPhotoController::class, 'index']);
+    Route::delete('/photos/{photo}', [ProgressPhotoController::class, 'destroy']);
+
+    // Coach endpoints
+    Route::get('/coach/clients/{user}/measurements', [CoachProgressController::class, 'clientMeasurements']);
+    Route::get('/coach/clients/{user}/photos', [CoachProgressController::class, 'clientPhotos']);
+
+});
